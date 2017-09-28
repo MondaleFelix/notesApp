7b3999ff58898d8e9e2bdf82c020b0650adc2860
Mondale Felix <mondalefelix@yahoo.com>
2017-09-28 14:03:04 -0700

    added table views

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // 2
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 3
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath)
+        
+        // 4
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        // 5
+        return cell
     }
     
 }
